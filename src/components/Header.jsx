c5a036b3231866b0888deee00585b4520a697dd2
David <david.irvineakamarv@gmail.com>
2024-01-31 13:49:38 +0700

    working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="p-4">
+      <div className="page-width">
+        <div className="p-4">
+          <span>MARV.DEV</span>
+        </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
